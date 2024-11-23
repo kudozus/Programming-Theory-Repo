245783a150b2e0bf711e752c7d0f8ae49eacb6b6
kudozus <joshuabrant@outlook.com>
2024-11-23 14:33:41 -0500

    Bug Fix
    -fixed scene flow
    -fixed issue with missing UI Event System

--- Programming Theory Project/Assets/Scripts/ReadInput.cs
+++ Programming Theory Project/Assets/Scripts/ReadInput.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ReadInput : MonoBehaviour
 {
@@ -22,5 +23,6 @@ public class ReadInput : MonoBehaviour
     {
         input = s;
         Debug.Log(input);
+        SceneManager.LoadScene(1);
     }
 }
